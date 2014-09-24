6f6e2ecec1b7e65a866c9ff48d5839b6ff0792d0
Adrian Price-Whelan <adrian.prw@gmail.com>
2014-09-23 22:00:58 -0400

    testing this shite

--- scripts/debris_actions.py
+++ scripts/debris_actions.py
@@ -28,8 +28,8 @@ from streamteam.units import galactic
 from streamteam.util import get_pool
 
 # Integration parameters
-nsteps = 100000
-dt = 0.4  # Myr
+nsteps = 200000
+dt = 0.75  # Myr
 
 def ic_generator(w0, mmap):
     n = 0
@@ -60,17 +60,25 @@ def main(file_path, output_path=None, mpi=False, overwrite=False):
         logger.debug("Creating path '{}'".format(output_path))
         os.mkdir(output_path)
 
-    time_file = os.path.join(output_path,"time_{}.npy".format(filename_base))
-    orbit_file = os.path.join(output_path,"orbits_{}.array".format(filename_base))
-    action_file = os.path.join(output_path,"actions_{}.npy".format(filename_base))
-    angle_file =os.path.join(output_path,"angles_{}.npy".format(filename_base))
-    freq_file = os.path.join(output_path,"freqs_{}.npy".format(filename_base))
+    files = dict()
+    files['time'] = os.path.join(output_path,"time_{}.npy".format(filename_base))
+    files['orbit'] = os.path.join(output_path,"orbits_{}.array".format(filename_base))
+    files['actions'] = os.path.join(output_path,"actions_{}.npy".format(filename_base))
+    files['angles'] =os.path.join(output_path,"angles_{}.npy".format(filename_base))
+    files['freqs'] = os.path.join(output_path,"freqs_{}.npy".format(filename_base))
+
+    if overwrite:
+        for fn in files.values():
+            if os.path.exists(fn):
+                logger.debug("Nuking {}".format(fn))
+                os.remove(fn)
 
     # TODO: create a reader for NBODY6 snapshots?
     # TODO: try reading with each reader?
     # Something like -- for reader in io.readers: ...
     scf = io.SCFReader(path)
     tbl = scf.read_snap(filename, units=galactic)
+    cen_w0 = io.tbl_to_w(scf.read_cen(units=galactic))[-1]
     w0 = np.squeeze(io.tbl_to_w(tbl))
     unbound = tbl['tub'] > 0.
 
@@ -78,15 +86,19 @@ def main(file_path, output_path=None, mpi=False, overwrite=False):
     w0 = w0[unbound]
     w0 = w0[np.random.randint(len(w0),size=norbits)]
 
+    # stack initial conditions for center of satellite on top
+    norbits += 1
+    w0 = np.vstack((cen_w0, w0))
+
     # potential = LM10Potential()
     potential = PW14Potential(q1=1.3, q3=0.8, phi=np.pi/2., theta=np.pi/2., psi=np.pi/2.)
 
     logger.info("Read initial conditions...")
-    if not os.path.exists(time_file):
+    if not os.path.exists(files['time']):
         logger.info("Beginning integration...")
 
         # create memory-mapped array to dump output to
-        mmap = np.memmap(orbit_file, mode='w+',
+        mmap = np.memmap(files['orbit'], mode='w+',
                          shape=(nsteps+1, norbits, 6), dtype=np.float64)
 
         if mpi:
@@ -97,19 +109,20 @@ def main(file_path, output_path=None, mpi=False, overwrite=False):
                                             dt=dt, nsteps=nsteps, mmap=mmap)
 
         logger.info("Saving to files...")
-        np.save(time_file, t)
-        w = np.memmap(orbit_file, mode='r',
+        np.save(files['time'], t)
+        w = np.memmap(files['orbit'], mode='r',
                       shape=(nsteps+1, norbits, 6), dtype=np.float64)
 
     else:
         logger.info("Files exist, reading orbit data...")
-        t = np.load(time_file)
-        w = np.memmap(orbit_file, mode='r',
+        t = np.load(files['time'])
+        w = np.memmap(files['orbit'], mode='r',
                       shape=(nsteps+1, norbits, 6), dtype=np.float64)
 
     logger.info("Orbit data loaded...")
 
     # Make a few orbit plots
+    np.random.seed(42)
     bnd = np.max(np.sqrt(np.sum(w[...,:3]**2, axis=-1)))
     for ix in np.random.randint(len(w0), size=10):
         ww = w[:,ix]
@@ -132,7 +145,7 @@ def main(file_path, output_path=None, mpi=False, overwrite=False):
     plt.savefig(os.path.join(output_path, "energy_cons_{}.png".format(filename_base)))
     logger.debug("Made energy conservation plot")
 
-    if not os.path.exists(action_file):
+    if not os.path.exists(files['actions']):
         logger.info("Computing actions...")
 
         # Compute actions, etc.
@@ -145,55 +158,45 @@ def main(file_path, output_path=None, mpi=False, overwrite=False):
             actions[i],angles[i],freqs[i] = sd.find_actions(t[::10], ww[::10],
                                                             N_max=6, usys=galactic)
 
-        np.save(action_file, actions)
-        np.save(angle_file, angles)
-        np.save(freq_file, freqs)
+        np.save(files['actions'], actions)
+        np.save(files['angles'], angles)
+        np.save(files['freqs'], freqs)
     else:
         logger.info("Reading actions/angles/freqs from files...")
-        actions = np.load(action_file)
-        angles = np.load(angle_file)
-        freqs = np.load(freq_file)
+        actions = np.load(files['actions'])
+        angles = np.load(files['angles'])
+        freqs = np.load(files['freqs'])
+
+    print(actions)
+    print(freqs)
 
     # Make frequency plot
-    # r1,r2,r3 = np.array([freqs[:,0]-np.mean(freqs[:,0]),
-    #                      freqs[:,1]-np.mean(freqs[:,1]),
-    #                      freqs[:,2]-np.mean(freqs[:,2])])*1000.
-    r1,r2,r3 = freqs.T*1000.
+    r1,r2,r3 = ((freqs[1:] - freqs[0])/freqs[0]).T
+    # r1,r2,r3 = freqs.T*1000.
 
     fig,axes = plt.subplots(1,2,figsize=(12,5),sharey=True,sharex=True)
     with mpl.rc_context({'lines.marker': '.', 'lines.linestyle': 'none'}):
-        axes[0].plot(r1[::2], r3[::2], alpha=0.25)
-        axes[1].plot(r2[::2], r3[::2], alpha=0.25)
+        axes[0].plot(r1, r3, alpha=0.25)
+        axes[1].plot(r2, r3, alpha=0.25)
 
-    axes[0].set_xlim(-1.,1.)
-    axes[0].set_ylim(-1.,1.)
-
-    # axes[0].set_xlabel(r"$\Omega_1-\langle\Omega_1\rangle$ [Gyr$^{-1}$]")
-    # axes[0].set_ylabel(r"$\Omega_3-\langle\Omega_3\rangle$ [Gyr$^{-1}$]")
-    # axes[1].set_xlabel(r"$\Omega_2-\langle\Omega_2\rangle$ [Gyr$^{-1}$]")
-    axes[0].set_xlabel(r"$\Omega_1$ [Gyr$^{-1}$]")
-    axes[0].set_ylabel(r"$\Omega_3$ [Gyr$^{-1}$]")
-    axes[1].set_xlabel(r"$\Omega_2$ [Gyr$^{-1}$]")
+    axes[0].set_xlabel(r"$(\Omega_1 - \Omega_{1,{\rm sat}})/\Omega_{1,{\rm sat}}$")
+    axes[1].set_xlabel(r"$(\Omega_2 - \Omega_{2,{\rm sat}})/\Omega_{2,{\rm sat}}$")
+    axes[0].set_ylabel(r"$(\Omega_3 - \Omega_{3,{\rm sat}})/\Omega_{3,{\rm sat}}$")
     fig.tight_layout()
     fig.savefig(os.path.join(output_path, "frequencies_{}.png".format(filename_base)))
 
     # Make action plot
-    # r1,r2,r3 = np.array([(actions[:,0]-np.mean(actions[:,0]))/np.mean(actions[:,0]),
-    #                      (actions[:,1]-np.mean(actions[:,1]))/np.mean(actions[:,1]),
-    #                      (actions[:,2]-np.mean(actions[:,2]))/np.mean(actions[:,2])])
-    r1,r2,r3 = actions.T
+    r1,r2,r3 = ((actions[1:] - actions[0])/actions[0]).T
+    # r1,r2,r3 = actions.T
 
     fig,axes = plt.subplots(1,2,figsize=(12,5),sharey=True)
     with mpl.rc_context({'lines.marker': '.', 'lines.linestyle': 'none'}):
-        axes[0].plot(r1[::2], r3[::2], alpha=0.25)
-        axes[1].plot(r2[::2], r3[::2], alpha=0.25)
+        axes[0].plot(r1, r3, alpha=0.25)
+        axes[1].plot(r2, r3, alpha=0.25)
 
-    # axes[0].set_xlabel(r"$(J_1-\langle J_1\rangle)/\langle J_1\rangle$")
-    # axes[0].set_ylabel(r"$(J_3-\langle J_3\rangle)/\langle J_3\rangle$")
-    # axes[1].set_xlabel(r"$(J_2-\langle J_2\rangle)/\langle J_2\rangle$")
-    axes[0].set_xlabel(r"$J_1$")
-    axes[0].set_ylabel(r"$J_3$")
-    axes[1].set_xlabel(r"$J_2$")
+    axes[0].set_xlabel(r"$(J_1 - J_{1,{\rm sat}})/J_{1,{\rm sat}}$")
+    axes[1].set_xlabel(r"$(J_2 - J_{2,{\rm sat}})/J_{2,{\rm sat}}$")
+    axes[0].set_ylabel(r"$(J_3 - J_{3,{\rm sat}})/J_{3,{\rm sat}}$")
     fig.tight_layout()
     fig.savefig(os.path.join(output_path, "actions_{}.png".format(filename_base)))
 
@@ -228,4 +231,4 @@ if __name__ == "__main__":
     else:
         logger.setLevel(logging.INFO)
 
-    main(args.filename, args.output, mpi=args.overwrite, overwrite=args.overwrite)
+    main(args.filename, args.output, mpi=args.mpi, overwrite=args.overwrite)
