fa64cfadc381534be97f4482a2b4eb6b524276ab
Adrian Price-Whelan <adrian.prw@gmail.com>
2015-01-12 14:55:28 -0500

    added anas log fit to via lactea

--- streammorphology/potential.py
+++ streammorphology/potential.py
@@ -4,6 +4,10 @@ from __future__ import division, print_function
 
 __author__ = "adrn <adrn@astro.columbia.edu>"
 
+# Third-party
+import astropy.units as u
+import numpy as np
+
 # Project
 import gary.potential as gp
 from gary.units import galactic
@@ -13,10 +17,19 @@ __all__ = ['potential_registry']
 # built-in potentials
 potential_registry = dict()
 
+# --------------------------------------------------------------
 p = gp.LeeSutoTriaxialNFWPotential(v_c=0.239225, r_s=30.,
                                    a=1., b=0.8, c=0.6,
                                    units=galactic)
 potential_registry['triaxial-NFW'] = p
 
+# --------------------------------------------------------------
 p = gp.TriaxialMWPotential(units=galactic)
 potential_registry['triaxial-NFW-disk-bulge'] = p
+
+# --------------------------------------------------------------
+p = gp.LogarithmicPotential(v_c=np.sqrt(2)*(140.372*u.km/u.s).decompose(galactic).value,
+                            r_h=5.87963,
+                            q1=0.872614, q2=1., q3=1.16395,
+                            phi=1.58374, units=galactic)
+potential_registry['via-lactea-log'] = p
