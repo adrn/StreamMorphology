6a6a76ff5274b01ae8570ae7b1a4153b8705100f
Adrian Price-Whelan <adrian.prw@gmail.com>
2015-01-27 14:34:44 -0500

    move worker func

---
+++ streammorphology/freqmap/mpi_util.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+
+""" Utilities for running frequency mapping with MPI (map) """
+
+from __future__ import division, print_function
+
+__author__ = "adrn <adrn@astro.columbia.edu>"
+
+# Standard library
+import os
+from collections import OrderedDict
+
+# Third-party
+import numpy as np
+from astropy.utils import isiterable
+
+# Project
+# ...
