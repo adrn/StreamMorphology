3e721f881e19beb19ea46c0f58f83b32f7c30522
Adrian Price-Whelan <adrian.prw@gmail.com>
2015-02-21 13:43:32 -0500

    remove imports

--- streammorphology/ensemble/core.py
+++ streammorphology/ensemble/core.py
@@ -4,15 +4,8 @@ from __future__ import division, print_function
 
 __author__ = "adrn <adrn@astro.columbia.edu>"
 
-# Standard library
-import os
-import sys
-
 # Third-party
 import numpy as np
-
-# Project
-# ...
 
 __all__ = ['create_ball']
 
