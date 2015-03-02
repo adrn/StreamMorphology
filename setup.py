dd6616c3d70542ed033ae02b919da9637a4af6a2
Adrian Price-Whelan <adrian.prw@gmail.com>
2015-03-02 09:38:41 -0500

    added setup to build cython

---
+++ setup.py
@@ -0,0 +1,47 @@
+# coding: utf-8
+
+from __future__ import division, print_function
+
+__author__ = "adrn <adrn@astro.columbia.edu>"
+
+# Standard library
+import os
+from distutils.core import setup
+from distutils.extension import Extension
+
+# Third-party
+import numpy as np
+from Cython.Distutils import build_ext
+from Cython.Build import cythonize
+
+# Get numpy path
+numpy_base_path = os.path.split(np.__file__)[0]
+numpy_incl_path = os.path.join(numpy_base_path, "core", "include")
+
+# Get gary path
+import gary
+gary_base_path = os.path.split(gary.__file__)[0]
+gary_incl_path = os.path.join(gary_base_path, "integrate", "dopri")
+
+extensions = []
+
+ensemble = Extension("streammorphology.ensemble.*",
+                     ["streammorphology/ensemble/*.pyx"],
+                     include_dirs=[numpy_incl_path,
+                                   gary_incl_path])
+extensions.append(ensemble)
+
+setup(
+    name="StreamMorphology",
+    version="0.1",
+    author="Adrian M. Price-Whelan",
+    author_email="adrn@astro.columbia.edu",
+    license="MIT",
+    cmdclass={'build_ext': build_ext},
+    ext_modules=cythonize(extensions),
+    packages=["streammorphology",
+              "streammorphology.ensemble",
+              "streammorphology.freqmap",
+              "streammorphology.lyapunov"],
+    scripts=[]
+)
