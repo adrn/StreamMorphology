c47de22dd55a75630e41907717ed4b5b18f536f5
Adrian Price-Whelan <adrian.prw@gmail.com>
2015-04-27 22:18:57 -0400

    remove individual status scripts

--- scripts/status.py
+++ scripts/status.py
@@ -1,28 +1,29 @@
 # coding: utf-8
 
-""" Check the status of Lyapunov mapping. """
+""" Check the status of frequency mapping. """
 
 from __future__ import division, print_function
 
 __author__ = "adrn <adrn@astro.columbia.edu>"
 
-from streammorphology.lyapunov import read_alllyap
+def main(path, subproject):
+    cmd = "from streammorphology.{0} import read, error_codes".format(subproject)
+    exec(cmd)
 
-def main(path):
-    # read allfreqs into structured array
-    d = read_alllyap(path)
+    # read cache file
+    d = read(path)
 
-    ndone = (d['lyap_exp'] != 0).sum()
+    ndone = (d['dt'] != 0.).sum()
     nsuccess = d['success'].sum()
-    nfail_energy = (d['error_code'] == 1).sum()
-    nfail_integrate = (d['error_code'] == 2).sum()
 
     print("Number of orbits: {}".format(len(d)))
     print("Done processing: {}".format(ndone))
     print("Successful: {}".format(nsuccess))
     print("Failures: {}".format(ndone-nsuccess))
-    print("\t Energy failure: {}".format(nfail_energy))
-    print("\t Integration failure: {}".format(nfail_integrate))
+
+    for ecode in sorted(error_codes.keys()):
+        nfail = (d['error_code'] == ecode).sum()
+        print("\t({0}) {1}: {2}".format(ecode, error_codes[ecode], nfail))
 
 if __name__ == '__main__':
     from argparse import ArgumentParser
@@ -32,7 +33,10 @@ if __name__ == '__main__':
     parser.add_argument("-p", "--path", dest="path", required=True,
                         help="Path to a Numpy memmap file containing the results "
                              "of frequency mapping.")
+    parser.add_argument("-s", "--subproject", dest="subproject", required=True,
+                        help="Name of the subproject to get reader and error "
+                             "codes from. e.g., 'freqmap'")
 
     args = parser.parse_args()
 
-    main(args.path)
+    main(args.path, subproject=args.subproject)
