4e2c2ebee537145de6dba74f6653797bd9b4ff52
Adrian Price-Whelan <adrian.prw@gmail.com>
2015-06-12 14:30:44 -0400

    added three orbits to package

--- streammorphology/__init__.py
+++ streammorphology/__init__.py
@@ -4,3 +4,9 @@ del os
 
 # Global parameters
 ETOL = 1E-7
+
+# Three orbits:
+import numpy as np
+three_orbits = {'mildly_chaotic':np.array([24.9,0.,19.7,0.,0.1403069,0.]),
+                'chaotic':np.array([24.9,0.,26.1,0.,0.09521978,0.]),
+                'regular':np.array([24.9,0.,6.9,0.,0.19509396,0.])}
