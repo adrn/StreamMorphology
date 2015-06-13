efe00f7015150772b07d97a4e856fdb1b262d444
Adrian Price-Whelan <adrian.prw@gmail.com>
2015-06-13 11:42:25 -0400

    switch to new, worse three orbits

--- streammorphology/__init__.py
+++ streammorphology/__init__.py
@@ -7,6 +7,10 @@ ETOL = 1E-7
 
 # Three orbits:
 import numpy as np
-three_orbits = {'mildly_chaotic':np.array([24.9,0.,19.7,0.,0.1403069,0.]),
-                'chaotic':np.array([24.9,0.,26.1,0.,0.09521978,0.]),
-                'regular':np.array([24.9,0.,6.9,0.,0.19509396,0.])}
+# three_orbits = {'mildly_chaotic':np.array([24.9,0.,19.7,0.,0.1403069,0.]),
+#                 'chaotic':np.array([24.9,0.,26.1,0.,0.09521978,0.]),
+#                 'regular':np.array([24.9,0.,6.9,0.,0.19509396,0.])}
+
+three_orbits = {'mildly_chaotic':np.array([27.9,0.,20.7,0.,0.1180944,0.]),
+                'chaotic':np.array([27.9,0.,21.9,0.,0.11030966,0.]),
+                'regular':np.array([27.9,0.,14.9,0.,0.1490257,0.])}
