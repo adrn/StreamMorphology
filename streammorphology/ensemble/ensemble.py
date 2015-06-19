423bf7b884a8cc4884f943804a76dd72ac63a7e1
Adrian Price-Whelan <adrian.prw@gmail.com>
2015-06-18 23:55:42 -0400

    restructure ensemble run

--- streammorphology/ensemble/ensemble.py
+++ streammorphology/ensemble/ensemble.py
@@ -6,13 +6,16 @@ from __future__ import division, print_function
 
 __author__ = "adrn <adrn@astro.columbia.edu>"
 
+# Std lib
+import sys
+
 # Third-party
 import numpy as np
 from astropy import log as logger
 from sklearn.neighbors import KernelDensity
 
 # Project
-from .core import create_ensemble
+from .core import create_ensemble, prepare_parent_orbit
 from .follow_ensemble import follow_ensemble
 from ..experimentrunner import OrbitGridExperiment
 
@@ -30,8 +33,8 @@ class Ensemble(OrbitGridExperiment):
     _run_kwargs = ['energy_tolerance', 'nperiods', 'nsteps_per_period',
                    'nensemble', 'mscale', 'kde_bandwidth', 'neval']
     config_defaults = dict(
-        energy_tolerance=1E-8, # Maximum allowed fractional energy difference
-        nperiods=100, # Total number of orbital periods to integrate for
+        energy_tolerance=1E-7, # Maximum allowed fractional energy difference
+        nperiods=16, # Total number of orbital periods to integrate for
         nsteps_per_period=512, # Number of steps per integration period for integration stepsize
         nensemble=1000, # How many orbits per ensemble
         mscale=1E4, # mass scale of the ensemble
@@ -56,40 +59,35 @@ class Ensemble(OrbitGridExperiment):
 
     @classmethod
     def run(cls, w0, potential, **kwargs):
-
-        config = dict()
+        c = dict()
         for k in cls.config_defaults.keys():
             if k not in kwargs:
-                config[k] = cls.config_defaults[k]
+                c[k] = cls.config_defaults[k]
             else:
-                config[k] = kwargs[k]
+                c[k] = kwargs[k]
 
         # container for return
         result = dict()
 
         try:
-            new_w0,dt,nsteps = prepare_parent_orbit(this_w0, potential,
-                                                    nperiods, nsteps_per_period)
+            new_w0,dt,nsteps = prepare_parent_orbit(w0.copy(), potential,
+                                                    c['nperiods'], c['nsteps_per_period'])
         except RuntimeError:
             logger.warning("Failed to integrate orbit when estimating dt,nsteps")
             result['success'] = False
             result['error_code'] = 1
             return result
 
-        logger.info("Orbit {}: initial dt={}, nsteps={}".format(index, dt, nsteps))
+        logger.debug("Integrating orbit with dt={0}, nsteps={1}".format(dt, nsteps))
 
         # create an ensemble of particles around this initial condition
-        ball_w0 = create_ensemble(new_w0, potential, N=nensemble, m_scale=mscale)
-        logger.debug("Generated ensemble of {0} particles".format(nensemble))
-
-        # get the initial density
-        kde = KernelDensity(kernel='epanechnikov', bandwidth=bw)
-        kde.fit(ball_w0[:,:3])
-        ball_dens0 = np.exp(kde.score_samples(ball_w0[:,:3]))
+        ensemble_w0 = create_ensemble(new_w0, potential, n=c['nensemble'], m_scale=c['mscale'])
+        logger.debug("Generated ensemble of {0} particles".format(c['nensemble']))
 
         try:
-            t, metric_d, ball_E = follow_ensemble(ball_w0, potential, dt, nsteps,
-                                                  nkld=nkld, kde_bandwidth=kd)
+            t, data, ball_E = follow_ensemble(ensemble_w0, potential, dt, nsteps,
+                                              neval=c['neval'],
+                                              kde_bandwidth=c['kde_bandwidth'])
         except:
             logger.warning("Unexpected failure: {0}".format(sys.exc_info()))
             result['success'] = False
@@ -97,36 +95,17 @@ class Ensemble(OrbitGridExperiment):
             return result
 
         dE_end = np.abs(ball_E[-1] - ball_E[0])
-        if (dE_end > ETOL).sum() > nensemble*0.1: # more than 10% don't meet criteria
+        if (dE_end > c['energy_tolerance']).sum() > c['nensemble']*0.1: # more than 10% don't meet criteria
             logger.warning("Failed due to energy conservation check.")
             result['success'] = False
             result['error_code'] = 3
             return result
 
-        # threshold defined as 1/10 initial mean density
-        thresh10 = ball_dens0.mean() / 10.
-        thresh100 = ball_dens0.mean() / 100.
-
-        # time at which mean density falls below threshold
-        ix = metric_d['mean'] < thresh10
-        try:
-            thresh_t_10 = t[ix][0]
-        except:
-            thresh_t_10 = np.nan
-
-        ix = metric_d['mean'] < thresh100
-        try:
-            thresh_t_100 = t[ix][0]
-        except:
-            thresh_t_100 = np.nan
-
-        result['thresh_t_10'] = thresh_t_10
-        result['thresh_t_100'] = thresh_t_100
         result['dt'] = dt
         result['nsteps'] = nsteps
         result['dE_max'] = dE_end.max()
         result['success'] = True
         result['error_code'] = 0
-        result['metrics_end'] = [metric_d[name][-1] for name in sorted(metric_d.dtype.names)]
+        result['mean_dens'] = data['mean']
 
         return result
