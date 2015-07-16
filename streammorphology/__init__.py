fa02a06afc131658bab32a6b851d495f1326d738
Adrian Price-Whelan <adrian.prw@gmail.com>
2015-07-16 14:47:27 -0400

    ordered dict

--- streammorphology/__init__.py
+++ streammorphology/__init__.py
@@ -5,12 +5,13 @@ del os
 # Three orbits:
 import numpy as np
 
-three_orbits = {
-    'near-resonant': np.array([17.0, 0.0, 26.060606060606062, 0.0, 0.12912205829404055, 0.0]), # resonant
-    'non-resonant': np.array([17.0, 0.0, 23.03030303030303, 0.0, 0.15198454276899373, 0.0]), # non-resonant
-    'weak-chaos': np.array([17.0, 0.0, 25.353535353535353, 0.0, 0.1346704105535305, 0.0]), # weak chaos
-    'strong-chaos': np.array([17.0, 0.0, 28.686868686868685, 0.0, 0.10691643457775891, 0.0]) # strong chaos
-}
+from collections import OrderedDict
+three_orbits = OrderedDict([
+    ('near-resonant', np.array([17.0, 0.0, 26.060606060606062, 0.0, 0.12912205829404055, 0.0])), # resonant
+    ('non-resonant', np.array([17.0, 0.0, 23.03030303030303, 0.0, 0.15198454276899373, 0.0])), # non-resonant
+    ('weak-chaos', np.array([17.0, 0.0, 25.353535353535353, 0.0, 0.1346704105535305, 0.0])), # weak chaos
+    ('strong-chaos', np.array([17.0, 0.0, 28.686868686868685, 0.0, 0.10691643457775891, 0.0])) # strong chaos
+])
 
 from experimentrunner import *
 from freqmap import Freqmap
